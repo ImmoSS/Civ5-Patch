4addd7083c4dd1c606fecb941a5b5a1498f99064
ImmoSS <83536444+ImmoSS@users.noreply.github.com>
2024-11-29 15:39:02 +0300

    FIX CS_ALLYING_WAR_RESCTRICTION on Coups

--- CvGameCoreDLL_Expansion2/CvEspionageClasses.cpp
+++ CvGameCoreDLL_Expansion2/CvEspionageClasses.cpp
@@ -2349,6 +2349,42 @@ bool CvPlayerEspionage::AttemptCoup(uint uiSpyIndex)
 		}
 		pNotifications->Add(eNotification, strNotification.toUTF8(), strSummary.toUTF8(), pCity->getX(), pCity->getY(), -1);
 	}
+
+#ifdef CS_ALLYING_WAR_RESCTRICTION
+	if (GC.getGame().isOption(GAMEOPTION_END_TURN_TIMER_ENABLED))
+	{
+		if (ePreviousAlly != NO_PLAYER)
+		{
+			if (ePreviousAlly != NO_PLAYER)
+			{
+				if (GET_PLAYER(ePreviousAlly).isHuman() && GET_PLAYER(m_pPlayer->GetID()).isHuman() && GET_PLAYER(ePreviousAlly).getTeam() != GET_PLAYER(m_pPlayer->GetID()).getTeam())
+				{
+					CvGame& kGame = GC.getGame();
+					float fGameTurnEnd = kGame.getPreviousTurnLen();
+					float fTimeElapsed = kGame.getTimeElapsed();
+					if (fGameTurnEnd - fTimeElapsed > CS_ALLYING_WAR_RESCTRICTION_TIMER)
+					{
+						GET_PLAYER(m_pPlayer->GetID()).setTurnCSWarAllowing(ePreviousAlly, kGame.getGameTurn());
+						GET_PLAYER(m_pPlayer->GetID()).setTimeCSWarAllowing(ePreviousAlly, fTimeElapsed + CS_ALLYING_WAR_RESCTRICTION_TIMER);
+					}
+					else
+					{
+						GET_PLAYER(m_pPlayer->GetID()).setTurnCSWarAllowing(ePreviousAlly, kGame.getGameTurn() + 1);
+						GET_PLAYER(m_pPlayer->GetID()).setTimeCSWarAllowing(ePreviousAlly, CS_ALLYING_WAR_RESCTRICTION_TIMER - (fGameTurnEnd - fTimeElapsed));
+					}
+				}
+			}
+			if (GET_PLAYER(ePreviousAlly).isHuman())
+			{
+				for (int iI = 0; iI < MAX_MAJOR_CIVS; iI++)
+				{
+					GET_PLAYER(ePreviousAlly).setTurnCSWarAllowing((PlayerTypes)iI, -1);
+					GET_PLAYER(ePreviousAlly).setTimeCSWarAllowing((PlayerTypes)iI, 0.f);
+				}
+			}
+		}
+	}
+#endif
 #ifdef REPLAY_EVENTS
 	std::vector<int> vArgs;
 	vArgs.push_back(static_cast<int>(uiSpyIndex));
