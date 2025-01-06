da3b1dae033b86743c8f1d972e5ca4fe15100b6f
ImmoSS <83536444+ImmoSS@users.noreply.github.com>
2025-01-06 23:46:14 +0300

    ADD UPDATE_UNIT_PROMOTIONS_ON_ACQUIRED_PLOT

--- CvGameCoreDLL_Expansion2/CvCity.cpp
+++ CvGameCoreDLL_Expansion2/CvCity.cpp
@@ -12272,6 +12272,37 @@ void CvCity::DoAcquirePlot(int iPlotX, int iPlotY)
 	pPlot->setOwner(getOwner(), GetID(), /*bCheckUnits*/ true, /*bUpdateResources*/ true);
 
 	DoUpdateCheapestPlotInfluence();
+
+#ifdef UPDATE_UNIT_PROMOTIONS_ON_ACQUIRED_PLOT
+	if (GET_TEAM(GET_PLAYER(getOwner()).getTeam()).canEmbark())
+	{
+		const IDInfo* pUnitNode;
+		UnitHandle pLoopUnit;
+
+		pUnitNode = pPlot->headUnitNode();
+
+		while (pUnitNode != NULL)
+		{
+			pLoopUnit = GetPlayerUnit(*pUnitNode);
+			pUnitNode = pPlot->nextUnitNode(pUnitNode);
+
+			if (pLoopUnit)
+			{
+				if (pLoopUnit->getOwner() == getOwner())
+				{
+					// Land Unit
+					if (pLoopUnit->getDomainType() == DOMAIN_LAND)
+					{
+						// Civilian unit or the unit can acquire this promotion
+						PromotionTypes ePromotionEmbarkation = GET_PLAYER(getOwner()).GetEmbarkationPromotion();
+						if (!pLoopUnit->IsCombatUnit() || ::IsPromotionValidForUnitCombatType(ePromotionEmbarkation, pLoopUnit->getUnitType()))
+							pLoopUnit->setHasPromotion(ePromotionEmbarkation, true);
+					}
+				}
+			}
+		}
+	}
+#endif
 }
 
 //	--------------------------------------------------------------------------------
