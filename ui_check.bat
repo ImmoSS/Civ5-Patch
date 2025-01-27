142df6229017b1453f4369b232f7e8e5f77c2b37
ImmoSS <83536444+ImmoSS@users.noreply.github.com>
2025-01-27 21:03:42 +0300

    10.5a

--- ui_check.bat
+++ ui_check.bat
@@ -1,7 +1,7 @@
 @ECHO off
 pushd "%~dp0"
 cd ..
-set patchfolder=Tournament Mod V10.5
+set patchfolder=Tournament Mod V10.5a
 del /q "%cd%\%patchfolder%\UI\"
 REM -------------------------------------------------
 copy /y "%cd%\%patchfolder%\tmp\ui\CultureOverview.lua" "%cd%\%patchfolder%\UI\CultureOverview.lua" > nul
