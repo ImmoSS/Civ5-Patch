1e60711dca3e1cec2600f90872b2f4ce27fed836
ImmoSS <83536444+ImmoSS@users.noreply.github.com>
2025-04-21 17:09:13 +0300

    FIX POLICY_FOE_TOURISM_MODIFIER

--- CvGameCoreDLL_Expansion2/CvCultureClasses.cpp
+++ CvGameCoreDLL_Expansion2/CvCultureClasses.cpp
@@ -3188,7 +3188,7 @@ CvString CvPlayerCulture::GetTourismModifierWithTooltip(PlayerTypes ePlayer) con
 	{
 		if (GET_TEAM(m_pPlayer->getTeam()).isAtWar(kPlayer.getTeam()))
 		{
-			szRtnValue += "[COLOR_POSITIVE_TEXT]" + GetLocalizedText("TXT_KEY_CO_PLAYER_TOURISM_FOE", iLessHappyMod) + "[ENDCOLOR]";
+			szRtnValue += "[COLOR_POSITIVE_TEXT]" + GetLocalizedText("TXT_KEY_CO_PLAYER_TOURISM_FOE", iFoeMod) + "[ENDCOLOR]";
 		}
 	}
 #endif
