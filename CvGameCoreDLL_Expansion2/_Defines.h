d6528727f155a12c667ab3bfecbce17176c22765
catscatsforever <44739966+catscatsforever@users.noreply.github.com>
2026-09-25 18:03:11 +0000

    FIX MAP FIX_UNIT_PROMOTION_POST_COMBAT_MESSAGE_VISIBILITY

--- CvGameCoreDLL_Expansion2/_Defines.h
+++ CvGameCoreDLL_Expansion2/_Defines.h
@@ -1145,6 +1145,8 @@
 #define FIX_SET_HAS_TECH_RESOURCE_CONNECTION
 ///
 #define FIX_IS_FRIENDLY_TERRITORY_MODIFIERS_AGAINST_CITIES
+// prevents post combat promotions (kris) text popups from being visible in unrevealed plots
+#define FIX_UNIT_PROMOTION_POST_COMBAT_MESSAGE_VISIBILITY
 /*BUGS FIXES END*/
 
 
